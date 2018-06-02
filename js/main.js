3ebaacec21e8d60ad12ddec15c3914a2e8e16a82
shenaan <shenaan@gmail.com>
2018-06-03 00:54:56 +0300

    finished homepage with responsive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
+    //mob menu
+    $("#header-mob__btn").click(function(){
+        $(this).toggleClass("active");
+        $(".header__nav").toggleClass('header__nav-open');
+        $("body").toggleClass('no-scroll');
+    });
+
+    $('.header__nav-link').click(function () {
+        $("#header-mob__btn").removeClass('active');
+        $(".header__nav").removeClass('header__nav-open');
+        $("body").removeClass('no-scroll');
+    });
+
     //slick slider
     $('.slick__slider').slick({
         arrows: false,
-        dots: true
+        dots: true,
+        fade: true
     });
+
 
 });
