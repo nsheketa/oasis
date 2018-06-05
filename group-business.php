5b0ef5c63c73c2a59797ecd6ea0c886920410706
shenaan <shenaan@gmail.com>
2018-06-05 23:12:04 +0300

    added group business pages

---
+++ group-business.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html class="no-js" lang="En-zh">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Group Business</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="slick/slick.css">
+    <link rel="stylesheet" href="slick/slick-theme.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-addon.css">
+    <link rel="stylesheet" href="css/stitches-1.3.5.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+<![endif]-->
+    <?php include(__DIR__ . '/include/header.php'); ?>
+
+    <main class="group-business__main main">
+        <div class="container">
+            <div class="padding-lr-30">
+                <div class="section__heading section__heading--blue">集團業務</div>
+            </div>
+        </div>
+
+        <div class="business__list group-business__list">
+            <div class="container">
+                <div class="row padding-lr-30">
+                    <div class="business__item col-12 col-md-6">
+                        <div class="business__item-inner">
+                            <div class="business__img">
+                                <img src="img/homepage/business-1.png" alt="Oasis group building" title="Oasis">
+                            </div>
+                            <div class="business__content-wrap d-flex">
+                                <div class="business__icon business__icon-home"></div>
+                                <div class="business__content">
+                                    <div class="h3 business__subtitle">美容服務</div>
+                                    <p class="business__text section__content--light">
+                                        集團的核心美容服務全歸納在「奧思」品牌下，構成一個具有協同效益的美容及健康相關服務業務組合。
+                                        整個奧思品牌系列提供市場獨有的全面服務及產品，得到幾乎所有重要消費者市場的青睞，
+                                        包括高、中端及大衆化市場的男女客戶，為他們提供全方位的綜合、專業及醫學美容服務。
+                                    </p>
+                                    <div class="group-business__btn-wrap">
+                                        <a href="#" class="button group-business__button">更多詳情</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="strategy-info__item-body d-flex flex-column justify-content-around align-items-center">
+                                <div class="strategy-info__item-row">
+                                    <div class="strategy-info__item-logo strategy__logo-oasis"></div>
+                                </div>
+                                <div class="strategy-info__item-row">
+                                    <div class="strategy-info__item-logo strategy__logo-medical"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-spa"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-beauty"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-homme"></div>
+                                </div>
+                                <div class="strategy-info__item-row">
+                                    <div class="strategy-info__item-logo strategy__logo-health"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-nail"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-florist"></div>
+                                </div>
+                                <div class="strategy-info__item-row margin-top-lg-80 margin-top-sm-40">
+                                    <div class="strategy-info__item-logo strategy__logo-glycel-sm"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="business__item group-business__item-2 col-12 col-md-6">
+                        <div class="business__item-inner">
+                            <div class="business__img">
+                                <img src="img/homepage/business-2.png" alt="Oasis group building" title="Oasis">
+                            </div>
+                            <div class="business__content-wrap d-flex">
+                                <div class="business__icon homepage-business__icon-globus"></div>
+                                <div class="business__content">
+                                    <div class="h3 business__subtitle">零售產品</div>
+                                    <p class="business__text section__content--light">
+                                        集團自營及分銷代理世界知名優質護膚品牌產品
+                                    </p>
+                                    <div class="group-business__btn-wrap">
+                                        <a href="#" class="button group-business__button">更多詳情</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="strategy-info__item-subtitle d-flex">
+                                <div class="col strategy-info__subtitle-text ">自營品牌</div>
+                                <div class="col strategy-info__subtitle-text ">代理品牌</div>
+                            </div>
+                            <div class="strategy-info__item-body d-flex">
+                                <div class="strategy-info__item-col d-flex flex-column justify-content-around align-items-center">
+                                    <div class="strategy-info__item-logo strategy__logo-glyvcel-md"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-eurobeauty"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-oasis-dermo"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-o-ko"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-o-pro"></div>
+                                </div>
+                                <div
+                                        class="strategy-info__item-col d-flex flex-column justify-content-center align-items-center">
+                                    <div class="strategy-info__item-logo strategy__logo-erno"></div>
+                                    <div class="strategy-info__item-logo strategy__logo-h2o"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include(__DIR__ . '/include/footer.php'); ?>
+
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="js/vendor/jquery-3.2.1.min.js"></script>
+<script src="js/vendor/bootstrap.bundle.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="slick/slick.min.js"></script>
+<script src="js/stitches-1.3.5.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
