91f444b21dd7e90604ceab6896b4c2ef632276ef
shenaan <shenaan@gmail.com>
2018-06-06 16:55:13 +0300

    added brands page

---
+++ brands.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<html class="no-js" lang="En-zh">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Management</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="slick/slick.css">
+    <link rel="stylesheet" href="slick/slick-theme.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-addon.css">
+    <link rel="stylesheet" href="css/stitches-1.3.5.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+<![endif]-->
+
+    <?php include(__DIR__ . '/include/header.php'); ?>
+
+    <?php include(__DIR__ . '/include/category.php'); ?>
+
+    <main class="management__main main">
+        <section class="management management-1">
+            <div class="container">
+                <div class="row padding-lr-110">
+                    <div class="col-12 col-md-4 col-lg-5">
+                        <div class="section__heading section__heading--white">
+                            執行董事 及 <br>
+                            行政總裁
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-7 management__list">
+                        <div class="row">
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-1.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">譚次生 先生</div>
+                                        <div class="management__item-desc"> 執行董事</div>
+                                        <div class="management-_item-desc"> 集團創辦人</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-2.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">譚次生 先生</div>
+                                        <div class="management__item-desc"> 執行董事</div>
+                                        <div class="management__item-desc"> 集團創辦人</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img" >
+                                    <img src="img/management/management-3.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">譚次生 先生</div>
+                                        <div class="management__item-desc"> 執行董事</div>
+                                        <div class="management__item-desc"> 集團創辦人</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-4.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">譚次生 先生</div>
+                                        <div class="management__item-desc"> 執行董事</div>
+                                        <div class="management__item-desc"> 集團創辦人</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-5.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">譚次生 先生</div>
+                                        <div class="management__item-desc"> 執行董事</div>
+                                        <div class="management__item-desc"> 集團創辦人</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="management management-2">
+            <div class="container">
+                <div class="row padding-lr-110">
+                    <div class="col-12 col-md-4 col-lg-5">
+                        <div class="section__heading section__heading--blue">
+                            獨立非執行董事
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-7 management__list">
+                        <div class="row">
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-5.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">黃龍德 教授</div>
+                                        <div class="management__item-desc">B.B.S.</div>
+                                        <div class="management__item-desc">太平紳士</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-4.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">黃龍德 教授</div>
+                                        <div class="management__item-desc">B.B.S.</div>
+                                        <div class="management__item-desc">太平紳士</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img" >
+                                    <img src="img/management/management-3.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">黃志強 博士</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="management management-3">
+            <div class="container">
+                <div class="row padding-lr-110">
+                    <div class="col-12 col-md-4 col-lg-5">
+                        <div class="section__heading section__heading--blue">
+                            高級管理人員
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-7 management__list">
+                        <div class="row">
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-1.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">歐滿盈 先生</div>
+                                        <div class="management__item-desc">首席財務官</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-sm-4 management__list-item">
+                                <div class="management__item-img">
+                                    <img src="img/management/management-2.png" alt="">
+                                    <div class="management__item-text d-flex flex-column justify-content-center">
+                                        <div class="management__item-name">喬慧基 女士</div>
+                                        <div class="management__item-desc">人力資源總監</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <?php include(__DIR__ . '/include/footer.php'); ?>
+
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="js/vendor/jquery-3.2.1.min.js"></script>
+<script src="js/vendor/bootstrap.bundle.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="slick/slick.min.js"></script>
+<script src="js/stitches-1.3.5.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
